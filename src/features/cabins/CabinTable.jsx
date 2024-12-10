1dfa5fc6584b6b925130555680daccd4f9ff100b
Trp Singh <trpsingh12@gmail.com>
2024-12-11 00:29:23 +0530

    filter property addotion , now we can filter the data of cabin by discount

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -4,34 +4,25 @@ import CabinRow from "./CabinRow.jsx";
 import { useCabins } from "./useCaabins.js";
 import Table from "../../ui/Table.jsx";
 import Menus from "../../ui/Menus.jsx";
+import { useSearchParams } from "react-router-dom";
 
-// const Table = styled.div`
-//   border: 1px solid var(--color-grey-200);
-
-//   font-size: 1.4rem;
-//   background-color: var(--color-grey-0);
-//   border-radius: 7px;
-//   overflow: hidden;
-// `;
-
-// const TableHeader = styled.header`
-//   display: grid;
-//   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-//   column-gap: 2.4rem;
-//   align-items: center;
-
-//   background-color: var(--color-grey-50);
-//   border-bottom: 1px solid var(--color-grey-100);
-//   text-transform: uppercase;
-//   letter-spacing: 0.4px;
-//   font-weight: 600;
-//   color: var(--color-grey-600);
-//   padding: 1.6rem 2.4rem;
-// `;
 
 function CabinTable() {
 
   const { isLoading, cabins } = useCabins();
+  const [searchParams] = useSearchParams();
+
+  const filterValues = searchParams.get('discount') || 'all';
+  let filteredCabin = [];
+  if (filterValues === 'all') {
+    filteredCabin = cabins;
+  }
+  if (filterValues === 'no-discount') {
+    filteredCabin = cabins?.filter(cabin => cabin.discount === 0);
+  }
+  if (filterValues === 'with-discount') {
+    filteredCabin = cabins?.filter(cabin => cabin.discount > 0);
+  }
 
   if (isLoading) return <Spinner />
   return (
@@ -46,7 +37,7 @@ function CabinTable() {
           <div>Discount</div>
           <div></div>
         </Table.Header>
-        <Table.Body data={cabins} render={(cabin) => (<CabinRow cabin={cabin} key={cabin.id} />)} />
+        <Table.Body data={filteredCabin} render={(cabin) => (<CabinRow cabin={cabin} key={cabin.id} />)} />
       </Table>
     </Menus>
   )
